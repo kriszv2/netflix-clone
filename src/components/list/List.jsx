f2b0ac6f88ce25179e9d5c3d7b884f025522b9cc
kriszv2 <krisztian.valyi1@gmail.com>
2022-08-22 11:29:27 +0100

    hover update

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,27 +1,50 @@
 import "./list.scss"
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import ListItem from "../listItem/listItem"
+import ListItem from "../listItem/ListItem"
+import { useRef, useState } from "react";
+
 
 export default function List() {
+    
+    const [slideNumber, setSlideNumber] = useState(0)
+
+    const listRef = useRef()
+    const handleClick = (direction) =>{
+        
+        
+        let distance = listRef.current.getBoundingClientRect().x
+        if(direction === "left" && slideNumber > 0){
+            setSlideNumber(slideNumber - 1)
+            
+            listRef.current.style.transform = `translateX(${(230 + distance)}px)`
+        }
+        if(direction === "right" && slideNumber < 2){
+            setSlideNumber(slideNumber + 1)
+            listRef.current.style.transform = `translateX(${-230 + distance}px)`
+        }
+    }
+
     return(
         <div className="list">
             <span className="listTitle">Continue to watch</span>
+            
             <div className="wrapper">
-                <ArrowBackIosIcon className="sliderArrow left"/>
-                <div className="container">
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-                    <ListItem/>
-
+            <ArrowBackIosIcon className="sliderArrow left" onClick={()=>handleClick("left")} />
+                <div className="container" ref={listRef} >
+                
+                    <ListItem index={0}/>
+                    <ListItem index={1}/>
+                    <ListItem index={2}/>
+                    <ListItem index={3}/>
+                    <ListItem index={4}/>
+                    <ListItem index={5}/>
+                    <ListItem index={6}/>
+                    <ListItem index={7}/>
+                    <ListItem index={8}/>
+                    <ListItem index={9}/>
                 </div>
-                <ArrowForwardIosIcon className="sliderArrow right"/>
+                <ArrowForwardIosIcon className="sliderArrow right" onClick={()=>handleClick("right")}/>
             </div>
         </div>
     )
--- src/components/listItem/listItem.jsx
+++ src/components/listItem/listItem.jsx
@@ -1,7 +1,47 @@
+import { PlayArrow } from "@mui/icons-material"
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOffAltOutlinedIcon from '@mui/icons-material/ThumbDownOffAltOutlined';
 import "./listItem.scss"
+import { useState } from "react";
 
-export default function listItem(){
+export default function ListItem({index}){
+
+    const [isHovered, setIsHovered] = useState(false)
+    const trailer = "https://www.youtube.com/embed/zAGVQLHvwOY";
+
     return(
-        <div className="listItem"></div>
+        <div
+      className="listItem"
+      style={{ left: isHovered && index * 325 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+            <img src="https://www.thewrap.com/wp-content/uploads/2022/06/joker.jpg" alt="" />
+            {isHovered && (
+                <>
+                <iframe src="https://www.youtube.com/embed/zAGVQLHvwOY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <div className="itemInfo">
+                <div className="icons">
+                    <PlayArrow className="icon"/>
+                    <AddIcon className="icon"/>
+                    <ThumbUpOutlinedIcon className="icon"/>
+                    <ThumbDownOffAltOutlinedIcon className="icon"/>
+                </div>
+                <div className="itemInfoTop">
+                    <span>1 hour 14 mins</span>
+                    <span className="limit">+18</span>
+                    <span>2020</span>
+                </div>
+                <div className="desc">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero 
+                expedita placeat mollitia repellendus cupiditate molestias iste soluta vero temporibus et quia, illo qui id
+                 possimus nemo fugiat doloremque illum ratione.
+                 </div>
+                 <div className="genre">Drama</div>
+            </div>
+            </>
+            )}
+            
+        </div>
     )
 }
